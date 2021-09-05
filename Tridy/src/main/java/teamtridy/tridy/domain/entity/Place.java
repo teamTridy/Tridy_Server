8af25ec6eacd30d77b2726e75cd11930820026c6
chaeppy <chaeppy108@gmail.com>
2021-09-05 20:48:38 +0900

    Feat: PlaceDetail Place Entity로 병합

--- Tridy/src/main/java/teamtridy/tridy/domain/entity/Place.java
+++ Tridy/src/main/java/teamtridy/tridy/domain/entity/Place.java
@@ -41,6 +41,24 @@ public class Place extends BaseEntity{
     @Column
     private String thumbImgUrl;
 
+    @Column//(columnDefinition = "comment '한줄 소개'")
+    private String intro;
+
+    @Column//(columnDefinition = "comment '개요 2줄 요약'")
+    private String story;
+
+    @Column//(columnDefinition = "comment '맵 주소'")
+    private String mapUrl;
+
+    @Column//(columnDefinition = "comment '대표 전화번호'")
+    private String rep;
+
+    @Column//(columnDefinition = "comment '상세 이용정보'")
+    private String info; //크롤링 할 것 고려
+
+    @Column//(columnDefinition = "comment '큰 이미지 주소'")
+    private String imgUrl; //크롤링 할 것 고려
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "location_id")
     private Location location;
@@ -60,5 +78,4 @@ public class Place extends BaseEntity{
     @OneToMany(mappedBy = "place")
     @Builder.Default
     private List<PlaceHashtag> placeHashtag = new ArrayList<>();
-
 }
