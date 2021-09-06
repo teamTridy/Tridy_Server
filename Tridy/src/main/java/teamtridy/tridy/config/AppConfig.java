5977bff1b5baa0ac21db8bec45450d95d7ff3546
chaeppy <chaeppy108@gmail.com>
2021-09-07 00:21:04 +0900

    Fix: JpaAuditing 미설정으로 시간 컬럼 입력 안되던 문제 해결

--- Tridy/src/main/java/teamtridy/tridy/config/AppConfig.java
+++ Tridy/src/main/java/teamtridy/tridy/config/AppConfig.java
@@ -3,6 +3,7 @@ package teamtridy.tridy.config;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.http.client.BufferingClientHttpRequestFactory;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.http.converter.StringHttpMessageConverter;
@@ -13,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
 import java.nio.charset.Charset;
 import java.time.Duration;
 
+@EnableJpaAuditing
 @Configuration
 public class AppConfig {
     @Bean
