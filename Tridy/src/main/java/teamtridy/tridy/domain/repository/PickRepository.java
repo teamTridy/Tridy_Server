1c588bd38afa3eb7c089b5fc75860c764588e070
chaeppy <chaeppy108@gmail.com>
2021-09-07 22:12:00 +0900

    Feat: 내 찜 목록 조회 API 추가 #14

--- Tridy/src/main/java/teamtridy/tridy/domain/repository/PickRepository.java
+++ Tridy/src/main/java/teamtridy/tridy/domain/repository/PickRepository.java
@@ -1,5 +1,7 @@
 package teamtridy.tridy.domain.repository;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import teamtridy.tridy.domain.entity.Account;
 import teamtridy.tridy.domain.entity.Pick;
@@ -8,4 +10,6 @@ import teamtridy.tridy.domain.entity.Place;
 public interface PickRepository extends JpaRepository<Pick, Long> {
 
     Pick findByAccountAndPlace(Account account, Place place);
+
+    Slice<Pick> findByAccountOrderByIdDesc(Account account, Pageable pageable);
 }
