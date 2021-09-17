5c7a33524a2fd3bf045e93cba8b86255533295e2
chaeppy <chaeppy108@gmail.com>
2021-09-17 13:38:30 +0900

    Feat: InterestRepository의 관심사 전체 조회에 Cache 추가 #39

--- Tridy/src/main/java/teamtridy/tridy/domain/repository/InterestRepository.java
+++ Tridy/src/main/java/teamtridy/tridy/domain/repository/InterestRepository.java
@@ -1,8 +1,12 @@
 package teamtridy.tridy.domain.repository;
 
+import java.util.List;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import teamtridy.tridy.domain.entity.Interest;
 
 public interface InterestRepository extends JpaRepository<Interest, Long> {
 
+    @Cacheable(value = "InterestFindAllCache")
+    List<Interest> findAll();
 }
